b757ef0c34459effab83c14fbc01b1ddbf6621bb
Vitor Xavier <vitorxa012@gmail.com>
2025-08-18 12:18:34 -0300

    Projeto Criado

---
+++ src/test/java/com/devvitor/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devvitor.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
